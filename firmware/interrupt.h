09087db24801228476447ce7a8ccaa396294d155
nkouvelas <nko@eworx.gr>
2015-11-19 23:30:37 +0200

    zczz

--- firmware/interrupt.h
+++ firmware/interrupt.h
@@ -1,7 +1,7 @@
 #ifndef _AVR_INTERRUPT_H_
 #define _AVR_INTERRUPT_H_
 
-#include <avr/io.h>
+#include "io.h"
 
 void sei(void); // Redefine the macro as a function so that it can be mocked by CMock
 void cli(void); // Redefine the macro as a function so that it can be mocked by CMock
